bc3ddfcbd57e7e331515a81a271cdd88f00cd8c5
KNHDSA_TV_YT <101498299+knhdsa@users.noreply.github.com>
2023-07-06 10:02:35 +0700

    Add files via upload

---
+++ program0.py
@@ -0,0 +1,49 @@
+import speech_recognition as sr
+import pyttsx3
+import os
+import keyboard
+from tkinter import *
+
+# Create a speech recognizer
+r = sr.Recognizer()
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+
+# Function for text-to-speech output
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+running = True  # Flag to indicate if the program is running
+
+while running:
+    if keyboard.is_pressed("ctrl+shift+f9"):
+        print("start")
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+
+            try:
+                text = r.recognize_google(audio, language='th')
+
+                print("Recognized Text:", text)  # Added print statement
+
+                if "เปิด Powerpoint" in text:
+                    os.system('start POWERPNT.EXE')
+
+                if "เปิด Word" in text:
+                    os.system("start WINWORD.EXE")
+
+                if "เปิด YouTube" in text:
+                    os.system("start https://youtube.com")
+
+                if "เปิด discord" in text or "เปิดดิสคอร์ด" in text:
+                    os.system('start discord')
+
+                if "เปิด Excel ":
+                    os.system("start Excel")
+
+            except sr.UnknownValueError:
+                print("ไม่สามารถรับรู้เสียงได้")
+            except sr.RequestError as e:
+                print("พบข้อผิดพลาดจากการร้องขอ:", str(e))
