ca6ca2c5f6377b0bdd13aa57092a3dc276535d3d
KNHDSA_TV_YT <101498299+knhdsa@users.noreply.github.com>
2023-07-06 11:01:50 +0700

    1.3

--- program0.py
+++ program0.py
@@ -23,10 +23,23 @@ def start():
             with sr.Microphone() as source:
                 audio = r.listen(source)
 
+
+def run():
+    while running:
+        if keyboard.is_pressed("ctrl+shift+f9"):
+            print("start")
+            with sr.Microphone() as source:
+                audio = r.listen(source)
+
                 try:
                     text = r.recognize_google(audio, language='th')
 
                     print("Recognized Text:", text)  # Added print statement
+
+                    print("Recognized Text:", text)  # Added print statement
+
+                    if "เปิด Powerpoint" in text:
+                        os.system('start POWERPNT.EXE')
 
                     if "เปิด Powerpoint" in text:
                         os.system('start POWERPNT.EXE')
@@ -34,14 +47,26 @@ def start():
                     if "เปิด Word" in text:
                         os.system("start WINWORD.EXE")
 
+                    if "เปิด Word" in text:
+                        os.system("start WINWORD.EXE")
+
+                    if "เปิด YouTube" in text:
+                        os.system("start https://youtube.com")
+
                     if "เปิด YouTube" in text:
                         os.system("start https://youtube.com")
 
                     if "เปิด discord" in text or "เปิดดิสคอร์ด" in text:
                         os.system('start discord')
 
+                    if "เปิด discord" in text or "เปิดดิสคอร์ด" in text:
+                        os.system('start discord')
+
                     if "เปิด Excel" in text:
                         os.system("start EXCEL.EXE")
+
+                    if "เปิด Excel " in text:
+                        os.system("start Excel")
 
                 except sr.UnknownValueError:
                     print("ไม่สามารถรับรู้เสียงได้")
