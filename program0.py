2091c8a99d52b05405318736797dc988b698d827
KNHDSA_TV_YT <nichapadit1976@gmail.com>
2023-07-06 10:40:24 +0700

    1.2

--- program0.py
+++ program0.py
@@ -16,34 +16,34 @@ def speak(text):
     engine.runAndWait()
 
 running = True  # Flag to indicate if the program is running
+def start():
+    while running:
+        if keyboard.is_pressed("ctrl+shift+f9"):
+            print("start")
+            with sr.Microphone() as source:
+                audio = r.listen(source)
 
-while running:
-    if keyboard.is_pressed("ctrl+shift+f9"):
-        print("start")
-        with sr.Microphone() as source:
-            audio = r.listen(source)
+                try:
+                    text = r.recognize_google(audio, language='th')
 
-            try:
-                text = r.recognize_google(audio, language='th')
+                    print("Recognized Text:", text)  # Added print statement
 
-                print("Recognized Text:", text)  # Added print statement
+                    if "เปิด Powerpoint" in text:
+                        os.system('start POWERPNT.EXE')
 
-                if "เปิด Powerpoint" in text:
-                    os.system('start POWERPNT.EXE')
+                    if "เปิด Word" in text:
+                        os.system("start WINWORD.EXE")
 
-                if "เปิด Word" in text:
-                    os.system("start WINWORD.EXE")
+                    if "เปิด YouTube" in text:
+                        os.system("start https://youtube.com")
 
-                if "เปิด YouTube" in text:
-                    os.system("start https://youtube.com")
+                    if "เปิด discord" in text or "เปิดดิสคอร์ด" in text:
+                        os.system('start discord')
 
-                if "เปิด discord" in text or "เปิดดิสคอร์ด" in text:
-                    os.system('start discord')
+                    if "เปิด Excel" in text:
+                        os.system("start EXCEL.EXE")
 
-                if "เปิด Excel ":
-                    os.system("start Excel")
-
-            except sr.UnknownValueError:
-                print("ไม่สามารถรับรู้เสียงได้")
-            except sr.RequestError as e:
-                print("พบข้อผิดพลาดจากการร้องขอ:", str(e))
+                except sr.UnknownValueError:
+                    print("ไม่สามารถรับรู้เสียงได้")
+                except sr.RequestError as e:
+                    print("พบข้อผิดพลาดจากการร้องขอ:", str(e))
