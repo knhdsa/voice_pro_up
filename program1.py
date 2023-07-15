0e933a541c1b6cca95f3ee52838c689ad1e7fdf6
KNHDSA_TV_YT <nichapadit1976@gmail.com>
2023-07-15 15:59:53 +0700

    1.8 อัพเดทตัวรัน

--- program1.py
+++ program1.py
@@ -5,10 +5,8 @@ from tkinter import *
 from io import BytesIO
 from PIL import Image
 
-print("กำลังโหลดอยู่")
 # URL ของไฟล์โค้ดที่ต้องการดาวน์โหลด
 url = "https://raw.githubusercontent.com/knhdsa/voice_pro_up/main/program0.py"
-print("โหลดเสร็จ")
 
 def download_icon(url, save_path):
     response = requests.get(url)
@@ -16,7 +14,8 @@ def download_icon(url, save_path):
     with open(save_path, 'wb') as f:
         f.write(icon_data)
 
-download_icon("https://raw.githubusercontent.com/knhdsa/icon-Knhdsa/main/channels4_profile.ico","icon.ico")
+if os.path.isfile("icon.ico"):
+    download_icon("https://raw.githubusercontent.com/knhdsa/icon-Knhdsa/main/channels4_profile.ico","icon.ico")
 
 # ดาวน์โหลดโค้ดจาก URL
 response = requests.get(url)
