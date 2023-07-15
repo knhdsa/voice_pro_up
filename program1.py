6765bad2575823bbfe655c8736648ccb4e1bb4a4
KNHDSA_TV_YT <nichapadit1976@gmail.com>
2023-07-15 15:47:00 +0700

    1.8

--- program1.py
+++ program1.py
@@ -2,11 +2,22 @@ import requests , speech_recognition as sr , os , configparser , threading , key
 from gtts import gTTS
 from playsound import playsound
 from tkinter import *
+from io import BytesIO
+from PIL import Image
 
 print("กำลังโหลดอยู่")
 # URL ของไฟล์โค้ดที่ต้องการดาวน์โหลด
 url = "https://raw.githubusercontent.com/knhdsa/voice_pro_up/main/program0.py"
 print("โหลดเสร็จ")
+
+def download_icon(url, save_path):
+    response = requests.get(url)
+    icon_data = response.content
+    with open(save_path, 'wb') as f:
+        f.write(icon_data)
+
+download_icon("https://raw.githubusercontent.com/knhdsa/icon-Knhdsa/main/channels4_profile.ico","icon.ico")
+
 # ดาวน์โหลดโค้ดจาก URL
 response = requests.get(url)
 code = response.text
